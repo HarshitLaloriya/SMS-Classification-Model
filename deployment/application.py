274248dfd2705fd69aa1dad7c0bafe9cc9844fcf
HarshitLaloriya <officialharshitlaloriya@gmail.com>
2024-07-05 22:09:29 +0530

    deployment completed

--- deployment/application.py
+++ deployment/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pickle, nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -22,17 +22,32 @@ def text_preprocess(text:str):
         
     return " ".join(stemed_words)
 
-
-
-@app.route(rule='/')
-def main():
+def spam_or_ham(data:str):
     tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
     model = pickle.load(open('model.pkl', 'rb'))
+    processed_data = text_preprocess(data)
+    print(data)
+    prediction = model.predict(tfidf.transform([processed_data]))
+    print(prediction[0])
+    output_pred = 'SPAM'
+    if prediction[0] == 0:
+        output_pred = 'HAM'
+        print(output_pred)
+        return output_pred
+    print(output_pred)
+    return output_pred
 
-    txt = "'hello i act acted acting actor 20% better ~`!@#$%^&*()_+=-{[\\|;:<>,./?\"\']}')"
-    print(text_preprocess(txt))
 
+@app.route(rule='/', methods=["GET", "POST"])
+def main():
+    print('in main function')
     return render_template("base.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    sms_input = request.get_json()['sms']
+    store_output = spam_or_ham(sms_input)
+    return jsonify({'output': store_output})
 
 if __name__ == "__main__":
     app.run(debug=True)
